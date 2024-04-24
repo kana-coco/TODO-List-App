e0f543c222b7e05c4dce0e0bac30f7c8123659b4
木下華菜子 <knk411ppp@gmail.com>
2024-04-24 18:06:01 +0900

    UPDATE:モデル定義、不要アプリケーション削除、アプリケーション名をtodolistに変更

---
+++ backend/my_django_project/todolist/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Task(models.Model):
+    # タスクの一意識別子(主キーid)：Djangoが自動で追加
+    # タスクのタイトル
+    title = models.CharField(max_length =200)
+    # タスクの詳細説明
+    description = models.TextField(brank=True, null=True)
+    # タスクが完了したかどうかを示すブールフィールド
+    completed = models.BooleanField(default=False)
+    # タスクの期限日
+    due_date = models.DateField(null=True, blank=True)
+    # タスクの作成日
+    created_at = models.DateTimeField(auto_now_add=True)
+    # タスクの更新日
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'tasks' # テーブル名
