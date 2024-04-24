73e0f791c041dc0c3c94b96a3f0324ef326c625b
木下華菜子 <knk411ppp@gmail.com>
2024-04-24 21:59:30 +0900

    UPDATE:Django管理サイトでデータを詳細に確認できるように修正

--- backend/my_django_project/todolist/admin.py
+++ backend/my_django_project/todolist/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from todolist.models import Task
 
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'completed', 'due_date', 'created_at', 'updated_at')  # 一覧に出したい項目
+    list_display_links = ('id', 'title',)  # 修正リンクでクリックできる項目
+
+admin.site.register(Task, TaskAdmin)
+
