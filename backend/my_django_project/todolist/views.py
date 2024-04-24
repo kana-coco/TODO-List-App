558f7b2e819ca6fa4e6ef623b57b5098594ea9d0
木下華菜子 <knk411ppp@gmail.com>
2024-04-24 22:57:38 +0900

    UPDATE:localhost:8000/task_list/にアクセスするとTask Listと表示される

--- backend/my_django_project/todolist/views.py
+++ backend/my_django_project/todolist/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 def task_list(request):
     # ToDo一覧を表示するロジック
-    return render(request, 'todolist/task_list.html')
+    return HttpResponse("Task List")
 
 def task_create(request):
-    # ToDo作成ページを表示するロジック 
-    return render(request, 'todolist/task_form.html')
+    # ToDo作成ページを表示するロジック
+    return HttpResponse("New Task")
