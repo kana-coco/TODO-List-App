e56428db0e0eb858cb267eeaff221e1efcd40092
木下華菜子 <knk411ppp@gmail.com>
2024-04-28 13:45:47 +0900

    ADD:reactコンポーネント追加

--- frontend/todo-react-app/src/App.js
+++ frontend/todo-react-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoList from './components/TodoList';
 
-function App() {
+const App = () => {
+  const todos = [
+    {id:1, title: 'Task1'},
+    {id:2, title: 'Task2'},
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>ToDo List</h1>
+        <TodoList todos={todos}/>
     </div>
   );
-}
+};
 
 export default App;
